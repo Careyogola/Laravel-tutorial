6cc934eb15b015a53779533bf5c4ea65255d5790
Carey Ogola <ogolacarey254@gmail.com>
2025-03-25 11:11:37 +0300

    Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h1>Welcome to Laravel</h1>
+<p>Some texts</p>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $person = [
+        'name'=>"John Doe",
+        "email"=> "abc@gmail.com",
+    ];
+    dump($person);
     return view('welcome');
 });
+
+Route::view('/about', 'about');
+
+Route::get('/products/{id}', function(string $id){
+    return "Product ID =$id";
+})->whereNumber('id');
+Route::get('/products/{category?}', function(string $category = null){
+    return "Product for category = $category";
+});
+
+Route::get('/users/{username}', function(string $username){
+    return "User = $username";
+})->whereAlpha("username");
